ebe20ee81226d71764455ce0b305e836da09377a
clkbrr <clkberraa@gmail.com>
2022-11-12 12:28:23 +0300

    create-react-app

---
+++ ecommerce-project/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <a href="/">My Store</a>
+      </header>
+      <main>list products</main>
+    </div>
+  );
+}
+
+export default App;
